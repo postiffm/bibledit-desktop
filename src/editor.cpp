5f4b177abd65cd15b62033b54cb72d52bbcadafb
Teus Benschop <teusjannette@gmail.com>
2011-05-21 18:23:47 +0200

    gtk: cursor in text editor gets placed at 0.33 of the height of the window

--- src/editor.cpp
+++ src/editor.cpp
@@ -1824,7 +1824,7 @@ void Editor2::scroll_insertion_point_on_screen_timeout() {
       insertion_point_offset += rectangle.y;
     }
 
-    // Set the adjustment to move the insertion point into the middle of the visible part of the window.
+    // Set the adjustment to move the insertion point into 1/3th of the visible part of the window.
     /*
     If the insertion point is at 800, and the height of the visible window is 500,
     and the total window height is 1000, then the calculation of the offset is as follows:
@@ -1833,7 +1833,7 @@ void Editor2::scroll_insertion_point_on_screen_timeout() {
     Therefore the adjustment should move to 550.
     The adjustment value should stay within its limits. If it exceeds these, the viewport draws double lines.
     */
-    gdouble adjustment_value = insertion_point_offset - (visible_window_height / 2);
+    gdouble adjustment_value = insertion_point_offset - (visible_window_height * 0.33);
     if (adjustment_value < 0) {
       adjustment_value = 0;
     }
