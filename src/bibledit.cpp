2bfc8ce63e278f324429d86ca42a3b961a4a9084
Teus Benschop <teusjannette@gmail.com>
2011-08-18 20:38:09 +0200

    bibledit-rdwrt: done

--- src/bibledit.cpp
+++ src/bibledit.cpp
@@ -30,7 +30,6 @@
 #include "mainwindow.h"
 #include "mappings.h"
 #include "ot-quotations.h"
-#include "readwrite.h"
 #include "runtime.h"
 #include "settings.h"
 #include "shell.h"
@@ -147,22 +146,9 @@ int main(int argc, char *argv[]) {
   upgrade();
   // Window icon fallback.
   gtk_window_set_default_icon_from_file(gw_build_filename(directories_get_package_data(), "bibledit.xpm").c_str(), NULL);
-  // Bibledit-gtk can read from or write to Bible data.
-  // Syntax: bibledit-gtk -r|-w ...
-  bool readdata = false;
-  bool writedata = false;
-  if (argc > 2) {
-    readdata = (strcmp(argv[1], "-r") == 0);
-    writedata = (strcmp(argv[1], "-w") == 0);
-  }
-  if (readdata ^ writedata) {
-    // Do the reading or the writing.
-    read_write_data(argc, argv, readdata, writedata);
-  } else {
-    // Start the gui if there are no scripting calls.
-    MainWindow mainwindow(xembed, accelerator_group);
-    gtk_main();
-  }
+  // Start the gui.
+  MainWindow mainwindow(xembed, accelerator_group);
+  gtk_main();
   // Destroy the accelerator group.
   g_object_unref(G_OBJECT(accelerator_group));
   // Clean up XML library.
