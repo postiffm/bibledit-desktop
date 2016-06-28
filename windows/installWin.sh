901edb0aae338b4166a37ab62ab1f2f434cc9dff
Matt Postiff <postiffm@umich.edu>
2016-06-27 20:42:38 -0400

    Upgrade installWin.sh for gtk3. Not yet complete.

--- windows/installWin.sh
+++ windows/installWin.sh
@@ -103,7 +103,8 @@ DEST_USRBIN="/c/$PROGRAMFILES/Bibledit-$VERSION/usr/bin"
 # Take note: this is the 64-bit version of stuff
 DLLS="/$MINGWDIR/bin"
 THEMES="/$MINGWDIR/share/themes"
-ENGINES="/$MINGWDIR/lib/gtk-2.0"
+ICONS="/$MINGWDIR/share/icons"
+ENGINES="/$MINGWDIR/lib/gtk-3.0"
 MINGWBIN="/$MINGWDIR/bin"
 USRBIN="/usr/bin"
 
@@ -391,11 +392,12 @@ mv -f "$ETC/fonts/conf.avail" "$ETC/fonts/conf.d"
 
 # The DLL ieshims.dll is a special case. depends.exe says we need it, but we don't.
 
-echo "Copying themes and engines..."
+echo "Copying icons, themes and engines..."
 mkdir -v -p "$SHARE/themes"
 cp -R $THEMES/* "$SHARE/themes/"
-mkdir -v -p "$LIB/gtk-2.0"
-cp -R $ENGINES/* "$LIB/gtk-2.0/"
+mkdir -v -p "$LIB/gtk-3.0"
+cp -R $ENGINES/* "$LIB/gtk-3.0/"
+cp -R $ICONS "$SHARE/"
 
 echo "Copying templates, etc. to $SHARE/bibledit..."
 mkdir -v -p "$SHARE/bibledit"
