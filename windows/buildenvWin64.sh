656d343c7062d78f3bdc8d3a47a30b30759aa3a0
Matt Postiff <postiffm@umich.edu>
2017-02-17 20:08:35 -0500

    Start renaming bibledit-gtk to bibledit-desktop

--- windows/buildenvWin64.sh
+++ windows/buildenvWin64.sh
@@ -64,8 +64,9 @@ pacman -S --noconfirm msys/libxml2-devel
 mkdir -v -p ~/$BUILD_DIR
 cd ~/$BUILD_DIR
 #git clone https://github.com/teusbenschop/bibledit.git
-git clone https://github.com/postiffm/bibledit-gtk.git
-echo "Git cloned into the directory ~/$BUILD_DIR/bibledit-gtk"
+#git clone https://github.com/postiffm/bibledit-gtk.git
+git clone https://github.com/postiffm/bibledit-desktop.git
+echo "Git cloned into the directory ~/$BUILD_DIR/bibledit-desktop"
 
 checkForZLIB1()
 {
