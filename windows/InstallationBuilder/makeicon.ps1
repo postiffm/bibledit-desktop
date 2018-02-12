8ea206a0c8a1e63f494e00bba7b90a9f6bf25e80
Matt Postiff <postiffm@umich.edu>
2018-02-12 10:42:02 -0800

    Change executable name in Windows desktop shortcut

--- windows/InstallationBuilder/makeicon.ps1
+++ windows/InstallationBuilder/makeicon.ps1
@@ -6,7 +6,7 @@ $path = 'C:\Program Files (x86)'
 if($bit -eq '64') {$path = 'C:\Program Files'}
 
 $shortcut = "$path\Bibledit-$version\Bibledit.lnk"
-$target = "$path\Bibledit-$version\editor\bin\bibledit-gtk.exe"
+$target = "$path\Bibledit-$version\editor\bin\bibledit-desktop.exe"
 $icon = "$path\Bibledit-$version\editor\bin\bibledit.ico"
 
 $ws = New-Object -ComObject WScript.Shell
