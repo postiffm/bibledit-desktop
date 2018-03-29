17e38a5fc4c7c268aa0974463a437b068e7ec1f4
Matt Postiff <postiffm@umich.edu>
2018-03-29 16:23:07 -0400

    Internationalization update
    Update bibledit-desktop.pot to new sources.
    Add capability to internationalize templates/stylesheet.xml
    with new .its and .loc files for xgettext.
    See po/README for instructions on how to make use of these new files.
    Update French translation file with latest sources.
    Now we need to update the French translation strings.

--- po/getStrings.sh
+++ po/getStrings.sh
@@ -2,3 +2,4 @@ xgettext -k_ ../src/*.cpp -o test.pot
 xgettext -j -k_ ../git/*.cpp -o test.pot
 xgettext -j -k_ ../shutdown/*.cpp -o test.pot
 xgettext -j --language=glade ../templates/gtkbuilder.*.xml -o test.pot
+xgettext -j ../templates/stylesheet.xml -o test.pot
