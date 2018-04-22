c7c271dce87566d88b7c096714663208008b2570
Matt Postiff <postiffm@umich.edu>
2018-04-22 16:57:52 -0400

    Minor fixes to French command batch file

--- windows/bibledit-fr.cmd
+++ windows/bibledit-fr.cmd
@@ -4,5 +4,5 @@ echo %LANGUAGE%
 echo "Param1=%1"
 echo "Param2=%2"
 echo "Param3=%3"
-#pause
-start "Bibledit" "bibledit-desktop.exe" %1 %2 %3
+REM pause
+start "Bibledit-Desktop" "%~dp0\bibledit-desktop.exe" %1 %2 %3
