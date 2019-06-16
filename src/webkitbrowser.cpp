b24714b7c62ed57327f99f24cde355c8273272c4
Scott Smith <pickle136@gmail.com>
2019-06-16 18:26:38 -0400

    New webkit function calls for clipboard commands (cut,copy,paste) , redo, undo

--- src/webkitbrowser.cpp
+++ src/webkitbrowser.cpp
@@ -1,20 +1,20 @@
 /*
  ** Copyright (©) 2003-2013 Teus Benschop.
- **  
+ **
  ** This program is free software; you can redistribute it and/or modify
  ** it under the terms of the GNU General Public License as published by
  ** the Free Software Foundation; either version 3 of the License, or
  ** (at your option) any later version.
- **  
+ **
  ** This program is distributed in the hope that it will be useful,
  ** but WITHOUT ANY WARRANTY; without even the implied warranty of
  ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  ** GNU General Public License for more details.
- **  
+ **
  ** You should have received a copy of the GNU General Public License
  ** along with this program; if not, write to the Free Software
  ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- **  
+ **
  */
 
 
@@ -192,7 +192,7 @@ bool WebkitBrowser::focused()
 void WebkitBrowser::copy()
 {
   if (focused()) {
-    webkit_web_view_copy_clipboard (WEBKIT_WEB_VIEW (webview));
+    webkit_web_view_execute_editing_command (WEBKIT_WEB_VIEW (webview), WEBKIT_EDITING_COMMAND_COPY);
   }
 }
 
