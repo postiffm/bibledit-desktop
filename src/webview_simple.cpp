e73b44c9fb3753069492e5430c9f6dddb666d303
Matt Postiff <postiffm@umich.edu>
2020-05-25 08:18:41 -0400

    Reformat switch stmt; add linux package for build

--- src/webview_simple.cpp
+++ src/webview_simple.cpp
@@ -19,9 +19,9 @@
 
 #include "webview_simple.h"
 
-void webview_simple::decide_policy_cb (WebKitWebView           *web_view,
-				       WebKitPolicyDecision    *decision,
-				       WebKitPolicyDecisionType decision_type)
+void webview_simple::decide_policy_cb(WebKitWebView *web_view,
+									  WebKitPolicyDecision *decision,
+									  WebKitPolicyDecisionType decision_type)
 {
 // Callback for clicking a link.
 #if 0
@@ -32,60 +32,63 @@ void webview_simple::decide_policy_cb (WebKitWebView           *web_view,
   
   DEBUG("remember old scroll position="+std::to_string(scrolling_position[active_url])+" for old active_url="+active_url)
 #endif
-    switch (decision_type) {
-    case WEBKIT_POLICY_DECISION_TYPE_NAVIGATION_ACTION:
-      {
-        WebKitNavigationPolicyDecision *navigation_decision = WEBKIT_NAVIGATION_POLICY_DECISION (decision);
-	WebKitNavigationAction         *navigation_action =
-	  webkit_navigation_policy_decision_get_navigation_action(navigation_decision);
-	WebKitNavigationType           navigation_type = 
-	  webkit_navigation_action_get_navigation_type(navigation_action);
-	
-	switch (navigation_type) {
-	case WEBKIT_NAVIGATION_TYPE_LINK_CLICKED: { // The navigation was triggered by clicking a link.
-	  // Don't follow pseudo-links clicked on this page.
-	  webkit_policy_decision_ignore (decision);
-	  
-	  WebKitURIRequest *request = webkit_navigation_action_get_request (navigation_action);
-	  const gchar *uri = webkit_uri_request_get_uri (request);
+	switch (decision_type)
+	{
+	case WEBKIT_POLICY_DECISION_TYPE_NAVIGATION_ACTION:
+	{
+		WebKitNavigationPolicyDecision *navigation_decision = WEBKIT_NAVIGATION_POLICY_DECISION(decision);
+		WebKitNavigationAction *navigation_action =
+			webkit_navigation_policy_decision_get_navigation_action(navigation_decision);
+		WebKitNavigationType navigation_type =
+			webkit_navigation_action_get_navigation_type(navigation_action);
 
-  	  // Load new page depending on the pseudo-link clicked.
-	  webview_process_navigation (uri);
+		switch (navigation_type)
+		{
+		case WEBKIT_NAVIGATION_TYPE_LINK_CLICKED:
+		{ // The navigation was triggered by clicking a link.
+			// Don't follow pseudo-links clicked on this page.
+			webkit_policy_decision_ignore(decision);
+
+			WebKitURIRequest *request = webkit_navigation_action_get_request(navigation_action);
+			const gchar *uri = webkit_uri_request_get_uri(request);
+
+			// Load new page depending on the pseudo-link clicked.
+			webview_process_navigation(uri);
+		}
+		break;
+		case WEBKIT_NAVIGATION_TYPE_FORM_SUBMITTED:	  // The navigation was triggered by submitting a form.
+													  // fall through
+		case WEBKIT_NAVIGATION_TYPE_BACK_FORWARD:	  // The navigation was triggered by navigating forward or backward.
+													  // fall through
+		case WEBKIT_NAVIGATION_TYPE_RELOAD:			  // The navigation was triggered by reloading.
+													  // fall through
+		case WEBKIT_NAVIGATION_TYPE_FORM_RESUBMITTED: // The navigation was triggered by resubmitting a form.
+													  // fall through
+		case WEBKIT_NAVIGATION_TYPE_OTHER:			  // The navigation was triggered by some other action.
+			webkit_policy_decision_use(decision);
+			break;
+		}
 	}
-	  break;
-	case WEBKIT_NAVIGATION_TYPE_FORM_SUBMITTED: // The navigation was triggered by submitting a form.
-	  // fall through
-	case WEBKIT_NAVIGATION_TYPE_BACK_FORWARD: // The navigation was triggered by navigating forward or backward.
-	  // fall through
-	case WEBKIT_NAVIGATION_TYPE_RELOAD: // The navigation was triggered by reloading.
-	  // fall through
-	case WEBKIT_NAVIGATION_TYPE_FORM_RESUBMITTED: // The navigation was triggered by resubmitting a form.
-	  // fall through
-	case WEBKIT_NAVIGATION_TYPE_OTHER: // The navigation was triggered by some other action.
-	  webkit_policy_decision_use (decision);
-	  break;
+	break;
+
+	case WEBKIT_POLICY_DECISION_TYPE_NEW_WINDOW_ACTION:
+	{
+		WebKitNavigationPolicyDecision *navigation_decision = WEBKIT_NAVIGATION_POLICY_DECISION(decision);
+		/* Make a policy decision here. */
+		webkit_policy_decision_use(decision);
 	}
-      }
-      break;
-	
-    case WEBKIT_POLICY_DECISION_TYPE_NEW_WINDOW_ACTION:
-      {
-        WebKitNavigationPolicyDecision *navigation_decision = WEBKIT_NAVIGATION_POLICY_DECISION (decision);
-        /* Make a policy decision here. */
-	webkit_policy_decision_use (decision);
-      }
-        break;
-    case WEBKIT_POLICY_DECISION_TYPE_RESPONSE:
-      {
-        WebKitResponsePolicyDecision *response = WEBKIT_RESPONSE_POLICY_DECISION (decision);
-        /* Make a policy decision here. */
-	webkit_policy_decision_use (decision);
-      }
-        break;
-    default:
-      /* Making no decision results in webkit_policy_decision_use(). */
-      return;
-    }
-  return;
-  // return value handled by the above callback func; maybe shouldn't be.
- }
+	break;
+	case WEBKIT_POLICY_DECISION_TYPE_RESPONSE:
+	{
+		WebKitResponsePolicyDecision *response = WEBKIT_RESPONSE_POLICY_DECISION(decision);
+		/* Make a policy decision here. */
+		webkit_policy_decision_use(decision);
+	}
+	break;
+	default:
+		/* Making no decision results in webkit_policy_decision_use(). */
+		return;
+	}
+	return;
+	// return value handled by the above callback func; maybe shouldn't be.
+}
