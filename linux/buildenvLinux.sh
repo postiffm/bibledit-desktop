5b846a47bd1642a4fa2f16847155e95b4b283265
lafricain79 <lafricain79@gmail.com>
2020-05-25 13:58:54 +0200

    Dep in the script for compilation on linux (Ubuntu) updated

--- linux/buildenvLinux.sh
+++ linux/buildenvLinux.sh
@@ -5,41 +5,36 @@ if [ "$EUID" -ne 0 ]; then
   exit
 fi
 
-echo "This script works on ubuntu Linux 17.04 LTS and Arch Linux"
+echo "This script works on ubuntu Linux 18.04 LTS and on Debian Unstable and Arch Linux"
 
 # I assume you have already done this:
-# sudo apt-get install git
+# sudo apt install git
 # cd ~
 # git clone https://github.com/postiffm/bibledit-desktop.git
 
-if apt-get --version 2> /dev/null; then
-  apt-get install gitk
-  apt-get install build-essential
-  #apt-get install libgtk2.0-dev
-  apt-get install yelp yelp-tools #Dependencies for documentation compilation
-  apt-get install libgtk-3-dev
-    
-  apt-get install rcs
-  apt-get install sqlite3
-  apt-get install libglibmm-2.4-dev libglibmm-2.4
-  apt-get install libsqlite3-dev
-  apt-get install libxml2-dev
-  apt-get install libenchant-dev
-  apt-get install libgtkhtml3.14-dev
-  #apt-get install libgtksourceview2.0-dev
-  apt-get install libgtksourceview-3.0
+if apt --version 2> /dev/null; then
+  apt install gitk
+  apt install build-essential
+  apt install autotools-dev
+  apt install yelp yelp-tools #Dependencies for documentation compilation
+  apt install libgtk-3-dev
+  apt install rcs
+  apt install sqlite3
+  apt install libglibmm-2.4-dev libglibmm-2.4-1v5
+  apt install libsqlite3-dev
+  apt install libxml2-dev
+  apt install libenchant-dev
+  apt install libgtksourceview-3.0-1 libgtksourceview-3.0-dev
   # Below installs -dev and -doc as well
-  apt-get install libwebkit2gtk-4.0
-  #apt-get install libwebkit-dev
-  #apt-get install libwebkitgtk-dev
-  apt-get install libwebkitgtk-3.0
-  apt-get install texlive-xetex
-  apt-get install php7.4-cli
-  apt-get install curl
-  apt-get install intltool
-  apt-get install libtool
-  apt-get install autoconf-archive
-  apt-get install gtk-3-examples
+  apt install libwebkit2gtk-4.0-37 libwebkit2gtk-4.0-dev
+#I'm not sure this package very heavy is still mandadory. BBD works perfectly without it.
+#  apt install texlive-xetex
+  apt install php7.4-cli
+  apt install curl
+  apt install intltool
+  apt install libtool
+  apt install autoconf-archive
+  apt install gtk-3-examples
 elif pacman --version 2> /dev/null; then
   # TODO: investigate if gtkhtml3 is necessary (it is in the AUR)
   pacman -S --needed --noconfirm base-devel gtk2 rcs sqlite \
@@ -49,16 +44,9 @@ elif pacman --version 2> /dev/null; then
 fi
 
 echo "You may also want to install development documentation if you are a developer."
-echo "apt-get install devhelp"
+echo "apt install devhelp"
 echo "sudo add-apt-repository ppa:p12/ppa"
-echo "sudo apt-get update"
-echo "sudo apt-get install cppreference-doc-en-html"
-echo "sudo apt-get install libxml2-doc"
-echo "sudo apt-get install libgtk2.0-doc"
-
-# This is an error that threw me for some reason for a while
-# % ./configure
-# checking for WEBKIT... no
-# configure: error: libwebkit development version >= 1.0.0 is needed.
-# What we need is this:
-#sudo apt-get install libwebkitgtk-dev
+echo "sudo apt update"
+echo "sudo apt install cppreference-doc-en-html"
+echo "sudo apt install libxml2-doc"
+echo "sudo apt install libgtk2.0-doc"
