839f0ca5c308507ffc64e03844ec8c09347f169d
Matt Postiff <postiffm@umich.edu>
2020-05-25 09:29:04 -0400

    Fix webkit2 signal handling in two more locations

--- src/windowreferences.cpp
+++ src/windowreferences.cpp
@@ -68,7 +68,6 @@ WindowReferences::WindowReferences(GtkWidget * parent_layout, GtkWindow *_transi
   
   connect_focus_signals (webview);
 
-  //g_signal_connect((gpointer) webview, "navigation-policy-decision-requested", G_CALLBACK(on_decide_policy_cb), gpointer(this));
   g_signal_connect((gpointer) webview, "decide-policy", G_CALLBACK(on_decide_policy_cb), gpointer(this));
   DEBUG("30")
   // Signal button.
--- src/windowshowrelatedverses.cpp
+++ src/windowshowrelatedverses.cpp
@@ -46,8 +46,7 @@ WindowShowRelatedVerses::WindowShowRelatedVerses(GtkWidget * parent_layout, GtkA
   
   connect_focus_signals (webview);
   
-  g_signal_connect((gpointer) webview, "navigation-policy-decision-requested", G_CALLBACK(on_decide_policy_cb), gpointer(this));
-
+  g_signal_connect((gpointer) webview, "decide-policy", G_CALLBACK(on_decide_policy_cb), gpointer(this));
   last_focused_widget = webview;
   gtk_widget_grab_focus (last_focused_widget);
   
